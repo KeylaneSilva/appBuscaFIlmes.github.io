93c1a46aa6e5d981c3751162b211dadc6b426383
Keylane <keylanessilva07@gmail.com>
2021-05-11 12:35:18 -0300

    terminando a parte de retorno dos detalhes dos filmes

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ function buscarFilme(filmePesquisa)
 {
     axios.get('http://www.omdbapi.com/?s=' + filmePesquisa + '&apikey=c42920a8')
   .then(function (response) {
-    console.log(response);
+    //console.log(response);
     var filmes = response.data.Search;
     var mostrarFilmes ='';
 
@@ -22,7 +22,7 @@ function buscarFilme(filmePesquisa)
                 <div class="thumbnail">
                     <img src="${filmes[i].Poster}" class="img-thumbnail">
                     <h4>${filmes[i].Title}</h4>
-                    <p><a href="#" class="btn btn-primary" role="button">Ver detalhes</a></p>
+                    <p><a href="#" class="btn btn-primary" role="button" onclick="filmeDetalhes('${filmes[i].imdbID}')">Ver detalhes</a></p>
                 </div>
             </div>
         `;
@@ -33,5 +33,54 @@ function buscarFilme(filmePesquisa)
   })
   .catch(function (error) {
     console.log(error);
+  });
+}
+
+function filmeDetalhes(id){
+  sessionStorage.setItem('filmesID', id);
+  window.location = 'detalhes.html';
+  return false;
+
+}
+
+function mostrarFilmes(){
+  var filmeID = sessionStorage.getItem('filmesID'); 
+  
+  axios.get('http://www.omdbapi.com/?i=' + filmeID + '&apikey=c42920a8')
+  .then(function (response) {
+    //console.log(response);
+    var filme = response.data;
+    console.log(filme);
+    var mostrarFilme = `
+      <div class="col-md-6">
+        <img src="${filme.Poster}" class="img-responsive">
+        <h3><strong>${filme.Title}</strong></h3>
+      </div>
+      <div class="col-md-6">
+          <div class="well clearfix">
+          <ul class="list-group">
+            <li class="list-group-item"><strong>Gênero: </strong> ${filme.Genre}</li>
+            <li class="list-group-item"><strong>Lançamento: </strong> ${filme.Released}</li>
+            <li class="list-group-item"><strong>Duração: </strong> ${filme.Runtime}</li>
+            <li class="list-group-item"><strong>Idioma: </strong> ${filme.Language}</li>
+            <li class="list-group-item"><strong>Prêmeios: </strong> ${filme.Awards}</li>
+            <li class="list-group-item"><strong>Atores: </strong> ${filme.Actors}</li>
+            <li class="list-group-item"><strong>Descrição: </strong> ${filme.Plot}</li>
+          </ul>
+
+          <hr>
+          <a href="http://imdb.com/title/${filme.imdbID}" target="_blank" class="btn btn-success" pull-left">Ver no IMDB</a>
+          <a href="index.html" target="_blank" class="btn btn-light" pull-right">Voltar a Pesquisa</a>
+
+          </div>
+      </div>
+
+    `;
+
+    document.getElementById('filmes').innerHTML = mostrarFilme;
+    
+  }).catch(function (error) {
+    console.log(error);
+    
   });
 }
